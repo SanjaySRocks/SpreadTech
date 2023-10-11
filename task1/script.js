9473296e98de469e4f962ccd56a4a106c21ece39
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2023-10-11 22:10:50 +0530

    added firebase database

--- task1/script.js
+++ task1/script.js
@@ -1,11 +1,36 @@
+// Firebase Config
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import { getDatabase, set, ref, child, push, query, orderByChild, equalTo, get } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCAn77TlRIPndZ8p9r0GLCPU1axjwM0hEk",
+  authDomain: "spreadtech-6438a.firebaseapp.com",
+  projectId: "spreadtech-6438a",
+  storageBucket: "spreadtech-6438a.appspot.com",
+  messagingSenderId: "1057692660223",
+  appId: "1:1057692660223:web:be08c9aab7ba9ac990f7ca"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase()
+const dbRef = ref(db,"users")
+
+// Get Form Data
 const Fname = document.getElementById("fname")
 const Lname = document.getElementById("lname")
 const Password = document.getElementById("pass")
 const ConfirmPassword = document.getElementById("cpass")
 const Email = document.getElementById("email")
 
+// Get Btn
 const btnSubmit = document.getElementById("submit")
 
+// Get btn click
 btnSubmit.addEventListener("click", function(e){
     e.preventDefault();
 
@@ -59,16 +84,56 @@ btnSubmit.addEventListener("click", function(e){
         password: hashPass
     }
 
-    localStorage.setItem("user", JSON.stringify(user))
+    // check if user exists or not
+    get(query(dbRef, orderByChild('email'), equalTo(user.email)))
+    .then((snapshot) => {
+        if (snapshot.exists()) {
+            setErrorMessage("Username already registered!")
+        } else {
+            // add user to database
+            addToFirebase(user)
+            .then(setSuccessMessage("User added successfully!"))
+            .catch((error) =>{
+                setErrorMessage(error);
+            });  
+        }
+    })
+    .catch((error) => {
+        console.error('Error checking username:', error);
+    });
 
-    setSuccess();
+    // localStorage.setItem("user", JSON.stringify(user))
+
+    
 });
 
-
-function setSuccess()
+function addToFirebase(user)
 {
-    const successMsg = document.querySelector(".registered-success")
-    successMsg.innerHTML = "Successfully registered!";
+    return new Promise((resolve, reject)=>{
+
+        const userid = push(child(ref(db), "users")).key;
+        set(ref(db, "users/" + userid), user)
+        .then( ()=> {resolve(); })
+        .catch((error)=>{ reject(error); });
+
+    })
+    
+}
+
+    
+
+function setErrorMessage(msg)
+{
+    const msgbox = document.querySelector(".registered-msg")
+    msgbox.className = "registered-msg registered-error"
+    msgbox.innerHTML = msg
+}
+
+function setSuccessMessage(msg)
+{
+    const msgbox = document.querySelector(".registered-msg")
+    msgbox.className = "registered-msg registered-success"
+    msgbox.innerHTML = msg
 
 }
 
